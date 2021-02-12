25e44f70af30142159f2006db6d8cf7ec3a3d5ee
doretox <doretox@protonmail.com>
2021-02-12 14:04:31 -0400

    chapter 3 first exercices

---
+++ Chapter 3/3-2-greetings.py
@@ -0,0 +1,13 @@
+#3-2. Greetings: Start with the list you used in Exercise 3-1, 
+#but instead of just printing each person’s name, print a message to them .
+#The text of each mes- sage should be the same, 
+#but each message should be personalized with the person’s name .
+
+names = ['Charlie Brown', 'Snoopy', 'Woodstock', 'Patty']
+
+message = "Hi, my name is "
+
+print(message + names[0] + ".")
+print(message + names[1] + ".")
+print(message + names[2] + ".")
+print(message + names[3] + ".")
---
+++ Chapter 3/3-3-your_own_list.py
@@ -0,0 +1,9 @@
+#3-3. Your Own List: Think of your favorite mode of transportation,
+# such as a motorcycle or a car, and make a list that stores several examples.
+# Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle .”
+
+transports = ["Tesla Model S", "Harley Davidson Dyna", "Ford Mustang"]
+
+print("I would like to own a " + transports[0] + ".")
+print ("I think a " + transports[1] + "is a insane motorcycle.")
+print("Nothing like a " + transports[2] + ".")
