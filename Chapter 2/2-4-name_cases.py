13770957b9e49a9222b29fd303f61bdeb500876d
doretox <doretox@kali-box>
2021-02-11 21:32:03 -0400

    Chapter 2 start

---
+++ Chapter 2/2-4-name_cases.py
@@ -0,0 +1,8 @@
+# 2-4. Name Cases: Store a person’s name in a variable, and then print that per-
+# son’s name in lowercase, uppercase, and titlecase.
+
+name = "john wick"
+
+print(name.lower())
+print(name.upper())
+print(name.title())
---
+++ Chapter 2/2-5-famous-quote.py
@@ -0,0 +1,6 @@
+# 2-5. Famous Quote: Find a quote from a famous person you admire. Print the
+# quote and the name of its author. Your output should look something like the
+# following, including the quotation marks:
+
+
+print("Elon Musk once said, " + '"When something is important enough, you do it even if the odds are not in your favor."')
