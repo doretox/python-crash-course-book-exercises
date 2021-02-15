281e6d3f1dd05cc47acb162c80e4bb0182497589
doretox <doretox@kali-box>
2021-02-15 09:43:27 -0400

    3-10

---
+++ Chapter 3/3-10-every_function.py
@@ -0,0 +1,24 @@
+# 3-10. Every Function: Think of something you could store in a list. For example,
+# you could make a list of mountains, rivers, countries, cities, languages, or any-
+# thing else you’d like. Write a program that creates a list containing these items
+# and then uses each function introduced in this chapter at least once.
+
+languages = ['C', 'PHP', 'Java', 'Python', 'JavaScript', 'C++', 'Ruby', 'Perl']
+
+#sorting the list
+languages.sort()
+print(languages)
+
+#sorting reverse
+languages.sort(reverse=True)
+print(languages)
+
+#sorting temporarily
+print(sorted(languages))
+
+#reverse order
+languages.reverse()
+print(languages)
+
+#printing the length of the list 
+print("The length of the list is: " + str(len(languages)) + " itens.")
