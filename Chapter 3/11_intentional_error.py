f454d2d595c38cff16519a66bfd6fb52e9bd951c
doretox <doretox@kali-box>
2021-02-15 09:57:44 -0400

    end of chapter 3

---
+++ Chapter 3/11_intentional_error.py
@@ -0,0 +1,10 @@
+# 3-11. Intentional Error: If you haven’t received an index error in one of your
+# programs yet, try to make one happen. Change an index in one of your pro-
+# grams to produce an index error. Make sure you correct the error before clos-
+# ing the program.
+
+# this code will not work #
+languages = ['C', 'PHP', 'Java', 'Python']
+
+# printing an index error
+print(languages[4])
