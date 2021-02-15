e84f9fb8bc9b604198338a896d5b39db187cc67f
doretox <doretox@kali-box>
2021-02-15 09:38:09 -0400

    chapter 3 more

---
+++ Chapter 3/3-9-dinner-guests.py
@@ -0,0 +1,8 @@
+# 3-9. Dinner Guests: Working with one of the programs from Exercises 3-4
+# through 3-7 (page 46), use len() to print a message indicating the number
+# of people you are inviting to dinner.
+
+guest_list = ['Richard Feynman', 'Elon Musk', 'Steve Jobs', 'Alan Turing', 'Isaac Newton', 'Nikola Tesla', 'Leonardo da Vinci']
+
+#You have to change the number to sting usign str() to be able to print it
+print("The number of guests are " + str(len(guest_list)) + ".")
