92fd750654a4d14f9f0a9ee4ada6f071d1f62b55
doretox <doretox@kali-box>
2021-02-17 14:09:57 -0400

    4-6

---
+++ Chapter 6/4_glossary_2.py
@@ -0,0 +1,23 @@
+# 6-4. Glossary 2: Now that you know how to loop through a dictionary, clean
+# up the code from Exercise 6-3 (page 102) by replacing your series of print
+# statements with a loop that runs through the dictionary’s keys and values.
+# When you’re sure that your loop works, add five more Python terms to your
+# glossary. When you run your program again, these new words and meanings
+# should automatically be included in the output.
+
+glossary = {
+    'string': 'A sequence of characters.',
+    'variable': 'Is a reserved memory location to store values.',
+    'list': 'Is a collection of arbitrary objects.',
+    'tuple': 'Is exactly like a list but its immutable.',
+    'print': 'A function that prints a specified message in the screen.',
+    'boolean expression': 'An expression that evaluates to True or False.',
+    'key': 'The first item in a key-value pair in a dictionary.',
+    'value': 'An item associated with a key in a dictionary.',
+    'comment': 'A note in a program that the Python interpreter ignores.',
+    'float': 'A numerical value with a decimal component.',
+} 
+
+for key, value in glossary.items():
+    print(key.title() + ": "+ value + "\n")
+
---
+++ Chapter 6/5_rivers.py
@@ -0,0 +1,28 @@
+# 6-5. Rivers: Make a dictionary containing three major rivers and the country
+# each river runs through. One key-value pair might be 'nile': 'egypt'.
+# •Use a loop to print a sentence about each river, such as The Nile runs
+# through Egypt.
+# •Use a loop to print the name of each river included in the dictionary.
+# •Use a loop to print the name of each country included in the dictionary.
+
+rivers = {
+    'nile': 'egypt',
+    'amazon': 'brazil',
+    'yangtze': 'china',
+}
+
+for key, value in rivers.items():
+    print("The " + key.title() + " runs through " + value.title())
+
+print("\n")
+
+print("These rivers are included in the dictionary: ")
+for key in rivers.keys():
+    print("- " + key.title())
+
+print("\n")
+
+print("These countries are included in the dictionary: ")
+for value in rivers.values():
+    print("- " + value.title())
+
