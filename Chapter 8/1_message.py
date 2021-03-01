c25d4d974276753d1d26bab5f62ae49b8960a65a
doretox <doretox@kali-box>
2021-03-01 08:42:42 -0400

    1-2 chapter 8

---
+++ Chapter 8/1_message.py
@@ -0,0 +1,10 @@
+# 8-1. Message: Write a function called display_message() that prints one sen-
+# tence telling everyone what you are learning about in this chapter. Call the
+# function, and make sure the message displays correctly.
+
+def display_message():
+    """Display a simple message."""
+    print("In this chpater I'm learning about Functions!!")
+
+# Calling the function
+display_message()
---
+++ Chapter 8/2_favorite_book.py
@@ -0,0 +1,10 @@
+# 8-2. Favorite Book: Write a function called favorite_book() that accepts one
+# parameter, title. The function should print a message, such as One of my
+# favorite books is Alice in Wonderland. Call the function, making sure to
+# include a book title as an argument in the function call.
+
+def favorite_book(book):
+    """Print the title of your favorite book."""
+    print("One of my favorite books is " + book.title() + "!")
+
+favorite_book('the lord of the rings')    
