aa507a3c7ff848f63a825372a8ccd1e9a0bcf9bc
doretox <doretox@protonmail.com>
2021-04-07 10:44:00 -0400

    12-14 chapter 8

---
+++ Chapter 8/12_sandwiches.py
@@ -0,0 +1,18 @@
+# 8-12. Sandwiches: Write a function that accepts a list of items a person wants on a sandwich. The function should have one parameter that collects as many items as the function call provides, and it should print a summary of the sandwich that is being ordered. Call the function three times, using a different number of arguments each time.
+
+def make_sandwich(*items):
+    """Build a sandwich with different items in it."""
+    print ("Making a sandwich with: ")
+    for item in items:
+        print("- " + item)
+
+
+make_sandwich('peperoni')
+
+print("\n")
+
+make_sandwich('cheese', 'ham')
+
+print("\n")
+
+make_sandwich('chicken', 'lettuce', 'tomato')
---
+++ Chapter 8/14_cars.py
@@ -0,0 +1,26 @@
+# 8-14. Cars: Write a function that stores information about a car in a diction-
+# ary. The function should always receive a manufacturer and a model name. It
+# should then accept an arbitrary number of keyword arguments. Call the func-
+# tion with the required information and two other name-value pairs, such as a
+# color or an optional feature. Your function should work for a call like this one:
+# car = make_car('subaru', 'outback', color='blue', tow_package=True)
+# Print the dictionary that’s returned to make sure all the information was
+# stored correctly.
+
+def make_car(manufacturer, model, **options):
+    """Make a dictionary representing a car."""
+    car = {
+        'manufacturer': manufacturer.title(),
+        'model': model.title(),
+        }
+    for option, value in options.items():
+        car[option] = value
+
+    return car
+
+outback = make_car('subaru', 'outback', color='blue', tow_package=True)
+print(outback)
+
+civic = make_car('honda', 'civic', year=2020, color='black',
+        headlights='popup')
+print(civic)
