0085fc6794d5db57cc79bca77cde4912a32d233f
doretox <doretox@protonmail.com>
2022-01-06 17:53:45 -0300

    ex 13 and 14

---
+++ Chapter 9/13_ordereddict_rewrite.py
@@ -0,0 +1,22 @@
+#9-13. OrderedDict Rewrite: Start with Exercise 6-4 (page 108), where you 
+#used a standard dictionary to represent a glossary. Rewrite the program using 
+#the OrderedDict class and make sure the order of the output matches the order 
+#in which key-value pairs were added to the dictionary.
+
+from collections import OrderedDict
+
+glossary = OrderedDict()
+
+glossary['string'] = 'A sequence of characters.'
+glossary['variable'] = 'Is a reserved memory location to store values.'
+glossary['list'] = 'Is a collection of arbitrary objects.'
+glossary['tuple'] = 'Is exactly like a list but its immutable.'
+glossary['print'] = 'A function that prints a specified message in the screen.'
+glossary['boolean expression'] = 'An expression that evaluates to True or False.'
+glossary['key'] = 'The first item in a key-value pair in a dictionary.'
+glossary['value'] = 'An item associated with a key in a dictionary.'
+glossary['comment'] = 'A note in a program that the Python interpreter ignores.'
+glossary['float'] = 'A numerical value with a decimal component.'
+
+for key, value in glossary.items():
+    print(key.title() + ": "+ value + "\n")
---
+++ Chapter 9/14_dice.py
@@ -0,0 +1,49 @@
+#9-14. Dice: The module random contains functions that generate random numbers #in a variety of ways. The function randint() returns an integer in the 
+#range you provide. The following code returns a number between 1 and 6:
+#from random import randint
+#x = randint(1, 6)
+#Make a class Dice with one attribute called sides, which has a default 
+#value of 6. Write a method called roll_dice() that prints a random number 
+#between 1 and the number of sides the dice has. Make a 6-sided die and roll 
+#it 10 times.
+#Make a 10-sided dice and a 20-sided dice. Roll each die 10 times.
+
+from random import randint
+
+class Dice():
+    """A class to represent a Dice."""
+
+    def __init__(self, sides=6):
+        """Initialize the dice."""
+        self.sides = sides
+
+    def roll_dice(self):
+        """Return a random number between 1 and the number of sides."""
+        return randint(1, self.sides)
+
+#make a 6 side dice and roll it 10 times
+d6 = Dice()
+results = []
+for roll_num in range(10):
+    result = d6.roll_dice()
+    results.append(result)
+print("10 rolls of a 6-sided dice:")
+print(results)
+
+#make a 10 side dice and roll it 10 times
+d10 = Dice(sides=20)
+results = []
+for roll_num in range(10):
+    result = d10.roll_dice()
+    results.append(result)
+print("10 rolls of a 10-sided dice:")
+print(results)
+
+#make a 20 side dice and roll it 10 times
+d20 = Dice(sides=20)
+results = []
+for roll_num in range(10):
+    result = d20.roll_dice()
+    results.append(result)
+print("10 rolls of a 20-sided dice:")
+print(results)
