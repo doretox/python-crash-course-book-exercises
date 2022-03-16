21a3434f00f9dc2f967fcb3c566ec409b1f19c95
doretox <doretox@protonmail.com>
2022-03-16 19:21:38 -0300

    end of chapter 14

--- Chapter 14/alien_invasion.py
+++ Chapter 14/alien_invasion.py
@@ -5,6 +5,8 @@ from settings import Settings
 from ship import Ship
 import game_functions as gf
 from game_stats import GameStats
+from button import Button
+from scoreboard import Scoreboard
 
 def run_game():
     # Initialize pygame, settings, and screen object.
@@ -13,8 +15,12 @@ def run_game():
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
-    # Create an instance to store game statistics.
+    # Make the play button
+    play_button = Button(ai_settings, screen, "Play")
+
+    # Create an instance to store game statistics and create a scoreboard
     stats = GameStats(ai_settings)
+    sb = Scoreboard(ai_settings, screen, stats)
 
     # Make a ship, a group of bullets and a group of aliens.
     ship = Ship(ai_settings, screen)
@@ -28,14 +34,14 @@ def run_game():
     while True:
 
         # Watch for keyboard and mouse events.
-        gf.check_events(ai_settings, screen, ship, bullets)
+        gf.check_events(ai_settings, screen, stats, sb, play_button, ship, aliens, bullets)
 
         if stats.game_active:                
             ship.update()            
-            gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
-            gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
+            gf.update_bullets(ai_settings, screen, stats, sb, ship, aliens, bullets)
+            gf.update_aliens(ai_settings, screen, stats, sb, ship, aliens, bullets)
 
         # Redraw the screen during each pass through the loop and make the most recently drown screen visible.
-        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
+        gf.update_screen(ai_settings, screen, stats, sb, ship, aliens, bullets, play_button)
 
 run_game()
